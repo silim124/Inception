c850e250814675818a64c9dd8f5471ca8dde3b1a
silim124 <siniim0124@gmail.com>
2022-04-05 02:03:55 +0900

    this is for test

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,13 +3,9 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 /usr/bin/mysqld -u root & sleep 1
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-# hostname `%` - 모든 호스트에서 서버에 연결
 # 새 사용자를 만들고 사용자에게 wp-db에 모든 권한을 부여
 mysql -u root -e "CREATE USER 'silim'@'localhost' IDENTIFIED BY 'silim';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'localhost';"
-
-mysql -u root -e "CREATE USER 'silim'@'%' IDENTIFIED BY 'silim';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'silim'@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"  # 변경 사항 저장 및 활성화
 
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'silim';";
